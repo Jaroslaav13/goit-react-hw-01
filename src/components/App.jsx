e6de5db3f06322c6306509b8dc40d24a7bd1f72e
Yaroslav <110483805+Jaroslaav13@users.noreply.github.com>
2024-02-07 15:36:50 +0200

    begin work

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import {Profile} from './Profile/Profile'
+export const App = () => {
+  return (
+    <>
+      <Profile
+        name={username}
+        tag={tag}
+        location={location}
+        image={avatar}
+        stats={stats}
+      />
+    </>
+  );
+};
