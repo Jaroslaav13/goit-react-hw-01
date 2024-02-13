86de8172c252a6cd971b92991801b7b16a567768
Yaroslav <110483805+Jaroslaav13@users.noreply.github.com>
2024-02-13 18:39:03 +0200

    task 1

---
+++ src/components/App/App.jsx
@@ -0,0 +1,18 @@
+import userData from '../userData.json';
+import Profile from '../Profile/Profile';
+
+export const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.name}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+        
+      />
+    </>
+  )
+}
+
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,32 +1,32 @@
-import userDate from '../userDate.json';
-
-export const Profile = ({username,tag,location,avatar}) => {
-    return (
-        <div>
-  <div>
-    <img
-      src={name}
+import css from './Profile.module.css'
+const Profile = ({name, tag, location, image, stats}) => {
+  return (
+<div className={css.container}>
+  <div className={css.userBox}>
+    <img className= {css.userPhoto}
+      src={image}
       alt="User avatar"
     />
-    <p>Petra Marica</p>
-    <p>@pmarica</p>
-    <p>Salvador, Brasil</p>
+        <p className= {css.textName}>{name}</p>
+        <p className= {css.textTag}>@{tag}</p>
+        <p className= {css.textLocation}>{location}</p>
   </div>
 
-  <ul>
-    <li>
-      <span>Followers</span>
-      <span>1000</span>
+  <ul className= {css.socList}>
+    <li className= {css.socItem}>
+      <span className= {css.activity}>Followers</span>
+          <span className={css.quantity}>{stats.followers}</span>
     </li>
-    <li>
-      <span>Views</span>
-      <span>2000</span>
+    <li className= {css.socItem}>
+      <span className= {css.activity}>Views</span>
+          <span className={css.quantity}>{stats.views}</span>
     </li>
-    <li>
-      <span>Likes</span>
-      <span>3000</span>
+    <li className= {css.socItem}>
+      <span className= {css.activity}>Likes</span>
+          <span className={css.quantity}>{stats.likes}</span>
     </li>
   </ul>
 </div>
-    )
+  )
 }
+export default Profile;
